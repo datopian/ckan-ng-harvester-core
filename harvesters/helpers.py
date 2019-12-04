718756b0fd4c7cff16d1f7a42b6443ae8efb3e9b
Andres Vazquez <andres@data99.com.ar>
2019-12-04 14:59:35 -0300

    clean tag_string. remove until upgrate CSW tests

--- harvesters/helpers.py
+++ harvesters/helpers.py
@@ -1,10 +1,9 @@
 import re
 
-print(pattern.sub('', text1))
 
 def clean_tags(tags):
     ret = []
-    pattern = re.compile('[^A-Za-z0-9\s_-!?]+')
+    pattern = re.compile('[^A-Za-z0-9\s_\-!?]+')
     for tag in tags:
         cleaned = pattern.sub('', tag)
         ret.append(cleaned.strip())
