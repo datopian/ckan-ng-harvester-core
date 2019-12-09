bbcb3f70937a9390115a8ca29b22d66bd5ad247f
Julie Kramer <thejuliekramer@gmail.com>
2019-12-09 16:06:57 -0600

    add test for validate_origin_distribution

--- tests/harvesters/datajson/test_resource.py
+++ tests/harvesters/datajson/test_resource.py
@@ -1,8 +1,26 @@
 import pytest
+import pytest
+from harvesters.datajson.ckan.resource import DataJSONDistribution
 
 class TestDataJSONResource(object):
 
-  def test_validate_origin_distribution(self):
+  def test_validate_origin_distribution(self, test_datajson_dataset):
+    original_resource = {'downloadURL': 'http://downloadURL.usda.gov/   '}
+    cra = DataJSONDistribution(original_resource=original_resource)
+    result = cra.validate_origin_distribution()
+    assert result == (True, None)
+
+    original_resource = {'accessURL': 'http://accessURL.usda.gov/   '}
+    cra = DataJSONDistribution(original_resource=original_resource)
+    result = cra.validate_origin_distribution()
+    assert result == (True, None)
+
+    original_resource = {}
+    cra = DataJSONDistribution(original_resource=original_resource)
+    result = cra.validate_origin_distribution()
+    assert result == (False, 'You need "downloadURL" or "accessURL" to conform a final url')
+
+  def test_validate_final_resource(self):
     pass
 
   def test_transform_to_ckan_resource(self):
