c61349b532a38c55393e97e33ee9d071d867abbf
Andres Vazquez <andres@data99.com.ar>
2019-12-11 17:27:37 -0300

    hotfix cleaning tags

--- harvesters/helpers.py
+++ harvesters/helpers.py
@@ -5,6 +5,7 @@ def clean_tags(tags):
     ret = []
     pattern = re.compile('[^A-Za-z0-9\s_\-!?]+')
     for tag in tags:
-        cleaned = pattern.sub('', tag)
-        ret.append(cleaned.strip())
+        cleaned = pattern.sub('', tag).strip()
+        if cleaned != '':
+            ret.append(cleaned)
     return ret
