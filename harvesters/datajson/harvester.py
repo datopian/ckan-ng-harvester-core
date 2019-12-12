eb097484373b3d84cea9fda152551d30988a6630
Andres Vazquez <andres@data99.com.ar>
2019-12-12 13:28:52 -0300

    fix tests and validation when reading from dict

--- harvesters/datajson/harvester.py
+++ harvesters/datajson/harvester.py
@@ -86,13 +86,15 @@ class DataJSON(HarvesterBaseSource):
         
         self.schema_version = VALID_DATAJSON_SCHEMAS[validator_schema]
         
-        try:
-            self.data_json = json.loads(self.raw_data_json)  # check for encoding errors
-        except Exception as e:
-            error = 'ERROR parsing JSON: {}. Data: {}'.format(e, self.raw_data_json)
-            self.errors.append(error)
-            logger.error(error)
-            return False
+        # check for encoding errors only if we not started this from a dict (as we do in some tests)
+        if self.raw_data_json is not None:
+            try:
+                self.data_json = json.loads(self.raw_data_json)
+            except Exception as e:
+                error = 'ERROR parsing JSON: {}. Data: {}'.format(e, self.raw_data_json)
+                self.errors.append(error)
+                logger.error(error)
+                return False
             
         error = None
 
